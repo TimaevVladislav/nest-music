8e99e608796e5c553fbf0fa08a76db85df47af87
timaevV <timaevvladislav@gmail.com>
2023-11-06 02:10:53 +0600

    feat:mongo with schema for module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
+import * as process from "process"
 import { Module } from "@nestjs/common"
+import {MongooseModule} from "@nestjs/mongoose"
+
 import { TracksModule } from "./tracks/tracks.module"
 
 @Module({
-  imports: [TracksModule],
+  imports: [
+      MongooseModule.forRoot("mongodb+srv://timaevvladislav:MRDUrBGibKcTOJLh@cluster0.vbi1pjc.mongodb.net/?retryWrites=true&w=majority"),
+      TracksModule
+  ],
   controllers: [],
   providers: []
 })
--- src/tracks/tracks.module.ts
+++ src/tracks/tracks.module.ts
@@ -1,9 +1,17 @@
 import { Module } from "@nestjs/common"
 import { TracksController } from "./tracks.controller"
 import { TracksService } from "./tracks.service"
+import {MongooseModule} from "@nestjs/mongoose"
+
+import {Track, TrackSchema} from "./schemas/track.schema"
+import {Comment, CommentSchema} from "./schemas/comment.schema"
 
 
 @Module({
+  imports: [
+     MongooseModule.forFeature([{name: Track.name, schema: TrackSchema}]),
+     MongooseModule.forFeature([{name: Comment.name, schema: CommentSchema}])
+  ],
   controllers: [TracksController],
   providers: [TracksService],
   exports: [TracksService]
